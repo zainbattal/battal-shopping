c3095d726a860e2faed18ac869a2d9ced6161f8e
zainbattal <battalzein@gmail.com>
2025-09-22 23:57:08 +0300

    test

--- client/src/components/get-products.jsx
+++ client/src/components/get-products.jsx
@@ -57,8 +57,7 @@ export default function GetProducts() {
   };
 
   const handleSave = async (id) => {
-    setSaveID(id);
-    console.log(saveID);
+    console.log("Saving ID:", id);
   };
 
   useEffect(() => {
