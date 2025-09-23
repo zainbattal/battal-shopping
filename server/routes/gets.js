668d0c1800f0904f28500ae3ca9c3ae078b1ba8f
zainbattal <battalzein@gmail.com>
2025-09-23 22:36:43 +0300

    icon

--- server/routes/gets.js
+++ server/routes/gets.js
@@ -1,17 +1,25 @@
 const router = require("express").Router();
+const { use } = require("react");
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
 
 router.post("/saveOne", async (req, res) => {
   try {
     const { id } = req.body;
+    const token = req.header("token");
+    const decoded = jwt.verify(token, process.env.jwtSecret);
+
+    const userRow = await pool.query("select * from users where user_id = $1", [
+      decoded.user,
+    ]);
+    const user = userRow.rows[0].user_name;
 
     const response = await pool.query(
       `UPDATE users
      SET saved_products = saved_products || $1
-     WHERE user_name = 'zezo'
+     WHERE user_name = $2
      AND NOT ($1 = ANY(saved_products));`,
-      [id]
+      [id, user]
     );
 
     res.json(response);
