6bddbd47074f26b960a77eac37017110639c05a7
zainbattal <battalzein@gmail.com>
2025-09-24 00:27:03 +0300

    test

--- server/routes/gets.js
+++ server/routes/gets.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const { use } = require("react");
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
 const jwt = require("jsonwebtoken");
@@ -31,7 +30,21 @@ router.post("/saveOne", async (req, res) => {
 
 router.get("/getSaved", async (req, res) => {
   try {
-    res.send("hi");
+    const token = req.header("token");
+    const decoded = jwt.verify(token, process.env.jwtSecret);
+
+    const userRow = await pool.query("select * from users where user_id = $1", [
+      decoded.user,
+    ]);
+
+    const user = userRow.rows[0].user_name;
+
+    const response = await pool.query(
+      `SELECT saved_products FROM users WHERE user_name = $1`,
+      [user]
+    );
+
+    res.json(response.rows[0]);
   } catch (error) {
     res.json(error.message);
   }
