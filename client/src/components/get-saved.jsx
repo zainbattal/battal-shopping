870cbb3e94cc75fdd8a68155957c3d7b9c2188ee
zainbattal <battalzein@gmail.com>
2025-09-24 14:24:18 +0300

    test

--- client/src/components/get-saved.jsx
+++ client/src/components/get-saved.jsx
@@ -23,7 +23,7 @@ export default function UserProducts() {
       let response2 = await fetch(
         "https://battal-shopping.onrender.com/gets/getProductsSaved",
         {
-          body: JSON.stringify({ data: data }),
+          body: JSON.stringify({ data2: data.saved_products }),
           method: "POST",
           headers: { "content-type": "application/json" },
         }
