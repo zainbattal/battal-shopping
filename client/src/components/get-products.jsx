f4683b2ceb05a7457ca132f9f991967a549fd9dd
zainbattal <battalzein@gmail.com>
2025-09-24 16:40:28 +0300

    hope it works

--- client/src/components/get-products.jsx
+++ client/src/components/get-products.jsx
@@ -63,7 +63,7 @@ export default function GetProducts() {
       "https://battal-shopping.onrender.com/gets/saveOne",
       {
         method: "POST",
-        body: JSON.stringify({ id: 9 }),
+        body: { id: id },
         headers: { "content-type": "application/json" },
       }
     );
